0c68c6fdd20e4b104691940d3c7e603ee7915f64
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-01 14:06:50 +0200

    Create Structs.cpp

---
+++ Structs.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+struct newt{
+    int n;
+    string s;
+    string ss;
+    int nn;
+    
+};
+
+int main() {
+    newt n1;
+    cin>>n1.n>>n1.s>>n1.ss>>n1.nn;
+    cout<<n1.n<<" "<<n1.s<<" "<<n1.ss<<" "<<n1.nn;
+    return 0;
+}
