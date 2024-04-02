4b261575329705031ffc7e400c317d9056ee1650
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-02 03:56:59 +0200

    Update Inheritance Introduction.cpp

--- Inheritance Introduction.cpp
+++ Inheritance Introduction.cpp
@@ -1,1 +1,34 @@
 
+#include <iostream>
+using namespace std;
+
+class Triangle
+{
+public:
+    void displayTriangle()
+    {
+        cout << "I am a triangle\n";
+    }
+};
+
+class Isosceles : public Triangle
+{
+public:
+    void displayIsosceles()
+    {
+        cout << "I am an isosceles triangle\n";
+    }
+
+    void displayDescription()
+    {
+        cout << "In an isosceles triangle two sides are equal\n";
+    }
+};
+
+int main() {
+    Isosceles isoTriangle;
+    isoTriangle.displayIsosceles();
+    isoTriangle.displayDescription();
+    isoTriangle.displayTriangle();
+    return 0;
+}
