8526faca19dccf45197c88e575165aeac2740cee
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 17:25:59 +0200

    Update Vector-Sort.cpp

--- Vector-Sort.cpp
+++ Vector-Sort.cpp
@@ -1,12 +1,12 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
 
 int main() {
+    FAST;
     int n;
     cin>>n;
     vector<int> v(n);
