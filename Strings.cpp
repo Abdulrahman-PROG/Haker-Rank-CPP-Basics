f64d15bf832585b417377cf75caffa07d4f3943b
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 17:24:27 +0200

    Update Strings.cpp

--- Strings.cpp
+++ Strings.cpp
@@ -1,20 +1,21 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
 
 
 int main() {
+    FAST;
     string a,b;
     cin>>a>>b;
     string s = a+b;
     char tmp = a[0];
     a[0] = b[0];
     b[0] = tmp;
-    cout<<a.size() <<" "<< b.size()<<endl;
-    cout<<s<<endl;
+    cout<<a.size() <<" "<< b.size()<<e;
+    cout<<s<<e;
     cout<<a<<" "<<b;
 
     return 0;
