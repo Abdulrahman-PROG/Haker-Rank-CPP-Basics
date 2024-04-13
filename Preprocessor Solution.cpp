85b878393cd45998263b0cfdb17001f96d2c9d86
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 17:16:33 +0200

    Update Preprocessor Solution.cpp

--- Preprocessor Solution.cpp
+++ Preprocessor Solution.cpp
@@ -1,5 +1,8 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
 #define INF 10000000 
 
@@ -22,6 +25,6 @@ int main() {
         min_val = MIN(min_val, arr[i]); 
     }
 
-    cout << "Result = " << max_val - min_val << endl; 
+    cout << "Result = " << max_val - min_val << FAST; 
     return 0;
 }
