aa21034a0ab6cab5673cd9ed7d8b1caa752f9f9d
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 17:24:48 +0200

    Update Structs.cpp

--- Structs.cpp
+++ Structs.cpp
@@ -1,11 +1,9 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
 
 struct newt{
     int n;
@@ -16,6 +14,7 @@ struct newt{
 };
 
 int main() {
+    FAST;
     newt n1;
     cin>>n1.n>>n1.s>>n1.ss>>n1.nn;
     cout<<n1.n<<" "<<n1.s<<" "<<n1.ss<<" "<<n1.nn;
