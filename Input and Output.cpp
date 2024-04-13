e78d77a6ff8f8ba7b66d1438ab8b9a01e3c56b4b
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 17:13:49 +0200

    Update Input and Output.cpp

--- Input and Output.cpp
+++ Input and Output.cpp
@@ -1,17 +1,15 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
 
 int main() {
+    FAST;
     int a,b,c;
     cin>>a>>b>>c;
     cout<<a+b+c;
   
-  
-
   return 0;
 }
