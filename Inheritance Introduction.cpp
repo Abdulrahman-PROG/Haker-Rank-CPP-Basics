1e97aed7b36c6226509a12caaa23667677513988
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 16:26:07 +0200

    Update Inheritance Introduction.cpp

--- Inheritance Introduction.cpp
+++ Inheritance Introduction.cpp
@@ -1,6 +1,8 @@
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
 class Triangle
 {
@@ -26,6 +28,7 @@ public:
 };
 
 int main() {
+    FAST;
     Isosceles isoTriangle;
     isoTriangle.displayIsosceles();
     isoTriangle.displayDescription();
