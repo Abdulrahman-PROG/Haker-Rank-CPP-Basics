b392b1f8aa04e12dbee0250800173cc49e5db8e3
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 16:07:51 +0200

    Update Basic Data Types.cpp

--- Basic Data Types.cpp
+++ Basic Data Types.cpp
@@ -1,18 +1,19 @@
-#include <iostream>
-#include <cstdio>
+#include <bits/stdc++.h>
 using namespace std;
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
 int main() {
+    FAST;
     int i;
     long l;
     char c;
     float f;
     double d;
 
-    // Input
     scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
 
-    // Output
     printf("%d\n%ld\n%c\n%.3f\n%.9lf", i, l, c, f, d);
 
     return 0;
