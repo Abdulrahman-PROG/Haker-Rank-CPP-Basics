be942bc2cb73357c40c50dc84f0b40f2c3e5f4e3
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 17:23:18 +0200

    Update Sets-STL.cpp

--- Sets-STL.cpp
+++ Sets-STL.cpp
@@ -1,11 +1,11 @@
-#include <cmath>
-#include <cstdio>
-#include <set>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
 int main() {
+    FAST;
     set<int> s;
     int q;
     cin >> q;
@@ -19,9 +19,9 @@ int main() {
         } else {
             auto itr = s.find(b);
             if(itr != s.end()) {
-                cout << "Yes" << endl;
+                cout << "Yes" << e;
             } else {
-                cout << "No" << endl;
+                cout << "No" << e;
             }
         }
     }
