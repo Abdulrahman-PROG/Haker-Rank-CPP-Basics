e39e5e301a45426d2140634a43a8550db0974cfd
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 17:16:10 +0200

    Update Pointer.cpp

--- Pointer.cpp
+++ Pointer.cpp
@@ -1,18 +1,19 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
 
 
 int main() {
+    FAST;
     int a,b;
     cin>>a>>b;
     int m = a-b;
     if (m < 0 ){
         m*=-1;
     }
-    cout<< a+b <<endl << m;
+    cout<< a+b <<e << m;
     return 0;
 }
