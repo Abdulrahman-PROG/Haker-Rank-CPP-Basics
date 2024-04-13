1835d1478c5acc937693c47341e3521353f33d15
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 16:20:22 +0200

    Update Conditional Statements.cpp

--- Conditional Statements.cpp
+++ Conditional Statements.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
 
 int main()
 {
-    
+    FAST;
     int n ;
     cin>>n;
     if(n == 1 ){
