4359bf46ca5bf1c66422be065c3556213354135f
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 17:24:03 +0200

    Update StringStream.cpp

--- StringStream.cpp
+++ StringStream.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <sstream>
+#include <bits/stdc++.h>
 using namespace std;
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
 vector<int> parseInts(string str) {
     vector<int> integers;
@@ -17,11 +18,12 @@ vector<int> parseInts(string str) {
 }
 
 int main() {
+    FAST;
     string str;
     cin >> str;
     vector<int> integers = parseInts(str);
     for (int num : integers) {
-        cout << num << endl;
+        cout << num << e;
     }
     return 0;
 }
