78d0b5b8d1d2bb19e657f0e1d53d7617bf096ca7
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 17:15:10 +0200

    Update Multi Level Inheritance .cpp

--- Multi Level Inheritance .cpp
+++ Multi Level Inheritance .cpp
@@ -1,28 +1,33 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
 
 class Triangle {
 public:
     void displayTriangle() {
-        cout << "I am a triangle" << endl;
+        cout << "I am a triangle" << e;
     }
 };
 
 class Isosceles : public Triangle {
 public:
     void displayIsosceles() {
-        cout << "I am an isosceles triangle" << endl;
+        cout << "I am an isosceles triangle" << e;
     }
 };
 
 class Equilateral : public Isosceles {
 public:
     void displayEquilateral() {
-        cout << "I am an equilateral triangle" << endl;
+        cout << "I am an equilateral triangle" << e;
     }
 };
 
 int main() {
+    FAST;
     Equilateral eqTriangle;
     eqTriangle.displayEquilateral();
     eqTriangle.displayIsosceles();
